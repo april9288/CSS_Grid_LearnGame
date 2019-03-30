7c4e320918fbc9be17095a7e36e31b507ffaab52
Jongho Kim <april9288@gmail.com>
2019-03-29 21:08:27 -0700

    Updates

---
+++ precache-manifest.6fe32d9986132187912ffa3b1f384394.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6feac0a1781db5ab1525c9d47456687",
+    "url": "/CssGridGame/static/media/dragon_yellow.b6feac0a.png"
+  },
+  {
+    "revision": "2c6f632419963f9810f9",
+    "url": "/CssGridGame/static/css/main.9f50ef6e.chunk.css"
+  },
+  {
+    "revision": "0c8ddd86d225736c7e4b",
+    "url": "/CssGridGame/static/js/runtime~main.9b8a61f3.js"
+  },
+  {
+    "revision": "849776d72e8dcb162a8f",
+    "url": "/CssGridGame/static/js/2.85591bdf.chunk.js"
+  },
+  {
+    "revision": "a410b60e4abd59ffe88d6298f5b938a4",
+    "url": "/CssGridGame/static/media/dragon_red.a410b60e.png"
+  },
+  {
+    "revision": "799103849ca0de43c3db2221dcf27d18",
+    "url": "/CssGridGame/static/media/fire_red.79910384.png"
+  },
+  {
+    "revision": "2c6f632419963f9810f9",
+    "url": "/CssGridGame/static/js/main.3823b28b.chunk.js"
+  },
+  {
+    "revision": "83a50bbba59e4f4e3ac463795bcf3340",
+    "url": "/CssGridGame/static/media/fire_yellow.83a50bbb.png"
+  },
+  {
+    "revision": "774a2bf7994ae74b1ccfdb70a02916e7",
+    "url": "/CssGridGame/static/media/ZTM.774a2bf7.jpg"
+  },
+  {
+    "revision": "8ef4fc981c63539a3b40b57946f6cb14",
+    "url": "/CssGridGame/static/media/dragon_blue.8ef4fc98.png"
+  },
+  {
+    "revision": "f4f4ccb51910eff8f275ef6b1d27e7b4",
+    "url": "/CssGridGame/static/media/fire_blue.f4f4ccb5.png"
+  },
+  {
+    "revision": "b5e2c2e9c22d5b0789fb67156aad3f96",
+    "url": "/CssGridGame/static/media/bg1.b5e2c2e9.jpg"
+  },
+  {
+    "revision": "cdc196c4ba95260c3942d786cec45e7c",
+    "url": "/CssGridGame/index.html"
+  }
+];
